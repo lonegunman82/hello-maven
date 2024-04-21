870a2d5c78a183cc95f5c84a0eb3b73865962dd8
Mateusz Pietrzyk <mateusz.pietrzyk@gmail.com>
2024-04-21 18:51:30 +0200

    Całość ćwieczeń z maven wykonana w domu.

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,17 +1,37 @@
 package pl.edu.agh.mwo.hellomaven;
 
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.elements.PseudoText;
+import com.indvd00m.ascii.render.Render;
+
+
 /**
- * Simple main application
+ * Simple main application.
  */
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+        final int  width = 120; //szerokość;
+        final int height = 20; //wysokość;
+        builder.width(width).height(height);
+        builder.element(new PseudoText("Hello World!"));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
+
+
+
     }
     
     public int getRandomNumber() {
         // chosen by fair dice roll.
         // guaranteed to be random
-        return 4;
+        final int number = 4;
+        return number; // czwórka;
     }
 }
